6a49973febfb999bed9d04157933c6f3e25fac73
ileies <admin@ileies.de>
2026-06-29 17:16:07 +0200

    Remove admin dashboard routes and shared components
    The admin dashboard (users, Minecraft, minechat, Discord) has moved to
    rizinos-web. Shared components that were only used there are also removed.

--- src/routes/admin/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import AdminNav from '$lib/components/AdminNav.svelte';
-	let { children } = $props();
-</script>
-
-<div class="min-h-screen bg-background">
-	<AdminNav />
-	{@render children()}
-</div>
